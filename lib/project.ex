b30a09716771e325afa2cffe578583954d4649bf
ely-uf <elyahove@student.unit.ua>
2020-05-23 00:54:49 +0300

    Add Project struct.

---
+++ lib/project.ex
@@ -0,0 +1,12 @@
+defmodule Project do
+  @type t :: %__MODULE__{
+    name: String.t()
+  }
+  @enforce_keys [:name]
+  defstruct [:name]
+
+  @spec new(String.t()) :: %Project{}
+  def new(name) do
+    %Project{name: name}
+  end
+end

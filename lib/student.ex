6bb0c7f17ac5ab2502891c1618a7f643c4c59fe7
Gregory <neogen570@gmail.com>
2020-05-23 00:09:10 +0300

    add dialyxir

--- lib/student.ex
+++ lib/student.ex
@@ -1,12 +1,14 @@
 defmodule Student do
   use GenServer
 
+  @spec new(String.t()) :: :ignore | {:error, any} | {:ok, pid}
   def new(name) do
     GenServer.start_link(__MODULE__, name)
   end
 
   @impl true
+  @spec init(String.t()) :: {:ok, %{name: String.t(), projects: []}}
   def init(name) do
-    { :ok, %{ name: name, projects: [] } }
+    {:ok, %{name: name, projects: []}}
   end
 end

ae289cb903d6e2427482b462b944bfe426ead08d
ely-uf <37557877+ely-uf@users.noreply.github.com>
2020-06-07 00:52:34 +0300

    Add WorldTime module. (#24)
    * Add WorldTime module.
    
    WorldTime is used to track a time within the simulation. The module
    allows to specify a multiplier to control the 'speed of time'.
    
    * Pass WorldTime through `mix format`.
    
    * Update lib/world_time.ex

---
+++ lib/world_time.ex
@@ -0,0 +1,39 @@
+defmodule WorldTime do
+  use Agent
+
+  @unit_epoch ~N[2016-11-16 08:42:00]
+
+  @type t :: %__MODULE__{
+          multiplier: number(),
+          start: NaiveDateTime.t()
+        }
+  @enforce_keys [:multiplier, :start]
+  defstruct [:multiplier, :start]
+
+  def start(multiplier) when multiplier <= 0, do: {:error, :badarg}
+
+  def start(multiplier) do
+    start = NaiveDateTime.utc_now()
+
+    Agent.start_link(
+      fn ->
+        %WorldTime{multiplier: multiplier, start: start}
+      end,
+      name: __MODULE__
+    )
+  end
+
+  def stop do
+    Agent.stop(__MODULE__)
+  end
+
+  def now() do
+    now = NaiveDateTime.utc_now()
+
+    Agent.get(__MODULE__, fn %WorldTime{multiplier: m, start: s} ->
+      NaiveDateTime.add(@unit_epoch, NaiveDateTime.diff(now, s) * m)
+    end)
+  end
+
+  def epoch, do: @unit_epoch
+end
